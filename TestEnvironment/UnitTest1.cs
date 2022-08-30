38c8664aa96eadb85f7d3c7ffb99600ee674eaf4
italopenha <italopenha77@outlook.com>
2022-08-30 11:04:57 -0300

    [Task: Created TestEnvironment] {30/08/2022 - 11:04}

---
+++ TestEnvironment/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestEnvironment
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
